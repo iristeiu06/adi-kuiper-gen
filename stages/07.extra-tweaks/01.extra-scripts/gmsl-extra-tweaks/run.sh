4426ba45282100e137dada35d0bcf538ef1a8cf4
Ioana Risteiu <Ioana.Risteiu@analog.com>
2025-09-16 19:56:33 +0300

    Install libcamera and copy xorg.conf.d file

---
+++ stages/07.extra-tweaks/01.extra-scripts/gmsl-extra-tweaks/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# kuiper2.0 - Embedded Linux for Analog Devices Products
+#
+# Copyright (c) 2024 Analog Devices, Inc.
+# Author: Larisa Radu <larisa.radu@analog.com>
+
+############## GUIDELINES ##############
+
+# - This script is run inside 'chroot'. 
+# 	* it works as if Kuiper is running on a system with a few limitations
+# 	* check 'chroot' documentation for more informations
+# - This script is run as root, there is no need to use 'sudo' command.
+# - Current directory is '/' (root) of the Kuiper rootfs.
+# - If a file needs to be copied, it should be placed inside 'adi-kuiper-gen'.
+# - If a variable from the configuration file is needed, source the config file to access its value.
+# - At this stage the Kuiper image is not yet partitioned. In order to modify what will be in the boot partition access /boot folder.
+# - This script will not be in the resulted image. If this is necessary it should be copied manually.
+
+############## EXAMPLES ##############
+
+# Source the config file to access configuration variables inside script
+# source config
+
+# Package installation
+# apt install -y <package>
+
+# Package installation without recommended packages (useful when Kuiper image size should be small)
+# apt install -y <package> --no-install-recommends
+
+# Copy a file placed in the examples folder to the current directory
+# cp stages/07.extra-scripts/examples/<file-to-be-copied> .
+
+# Enable a service 
+# systemctl enable <service>.service
+
+install -m 644 "${BASH_SOURCE%%/run.sh}"/files/99-v3d.conf "${BUILD_DIR}/etc/X11/xorg.conf.d"
+
+apt install -y python-pip git python3-jinja2
+
+# install the libcamera dependencies
+apt install -y libboost-dev
+apt install -y libgnutls28-dev openssl libtiff-dev pybind11-dev
+apt install -y qtbase5-dev libqt5core5a libqt5widgets
+apt install -y meson cmake
+apt install -y python3-yaml python3-ply
+apt install -y libglib2.0-dev libgstreamer-plugins-base1.0-dev
+
+git clone https://github.com/raspberrypi/libcamera.git
+cd libcamera
+meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
+ninja -C build install
