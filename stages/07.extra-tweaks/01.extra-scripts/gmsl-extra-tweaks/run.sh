b32a449e7b2d4ee66e304f105d0e3d846a86bf1d
Ioana Risteiu <Ioana.Risteiu@analog.com>
2025-09-17 18:39:41 +0300

    Install extra packages

--- stages/07.extra-tweaks/01.extra-scripts/gmsl-extra-tweaks/run.sh
+++ stages/07.extra-tweaks/01.extra-scripts/gmsl-extra-tweaks/run.sh
@@ -37,17 +37,23 @@
 
 install -m 644 "${BASH_SOURCE%%/run.sh}"/files/99-v3d.conf "${BUILD_DIR}/etc/X11/xorg.conf.d"
 
-apt install -y python-pip git python3-jinja2
+# 1. packages:
+apt install v4l-utils qv4l2
 
-# install the libcamera dependencies
-apt install -y libboost-dev
-apt install -y libgnutls28-dev openssl libtiff-dev pybind11-dev
-apt install -y qtbase5-dev libqt5core5a libqt5widgets
-apt install -y meson cmake
-apt install -y python3-yaml python3-ply
-apt install -y libglib2.0-dev libgstreamer-plugins-base1.0-dev
+apt install -y python3-pip git python3-jinja2 libboost-dev libgnutls28-dev openssl libtiff-dev pybind11-dev \
+               qtbase5-dev libqt5core5a libqt5widgets meson cmake python3-yaml python3-ply libglib2.0-dev \
+               libgstreamer-plugins-base1.0-dev g++ ninja-build pkg-config libyaml-dev libdw-dev libunwind-dev \
+               libudev-dev libgstreamer1.0-dev libpython3-dev libevent-dev libdrm-dev libtiff-dev qt6-base-dev
 
 git clone https://github.com/raspberrypi/libcamera.git
 cd libcamera
 meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
 ninja -C build install
+
+# 2.python
+pip install pixutils pykms pyv4l2
+
+# 3. extra configs:
+
+# Set the name of the machine to 'analog-rpi-gmsl'
+echo "analog-rpi-gmsl" > /etc/hostname
